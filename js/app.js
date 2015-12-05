cf9c0bab69acf20cb1d299901381dbebeb53a572
折腾笔记 <guoshengqiang@live.com>
2015-12-05 17:34:56 +0800

    版本初建

---
+++ js/app.js
@@ -0,0 +1,108 @@
+$(function() {
+
+	$('#imgFile').on('change', function() {
+		$('#fileNum').html('点击重选文件(共'+this.files.length+'张) 鼠标停留显示文件列表');
+	});
+	$('#imgType').on('change',function(){
+		if($(this).val()==='jpg'){
+			$('#blockquote-quality').addClass('blockquote-info');
+		}else{
+			$('#blockquote-quality').removeClass('blockquote-info');
+		}
+	})
+	$('#start').on('click',function(){
+		start($('#imgFile')[0].files);
+	});
+});
+var IMGTYPE = {
+	png: 'image/png',
+	jpg: 'image/jpeg',
+}
+
+function dataURLtoBlob(dataurl) { 
+	var arr = dataurl.split(','),
+		mime = arr[0].match(/:(.*?);/)[1],
+		bstr = atob(arr[1]),
+		n = bstr.length,
+		u8arr = new Uint8Array(n);
+	while (n--) {
+		u8arr[n] = bstr.charCodeAt(n);
+	}
+	return new Blob([u8arr], {
+		type: mime
+	});
+}
+
+function start(files_) {
+	var files=[];
+	var drawN = 0;
+	var canvas = document.createElement('canvas');
+	var canvas2d = canvas.getContext("2d");
+	var direction = $('#direction').val();
+	var sortType = $('#sortType').val();
+	if (sortType === 'name1' || sortType === 'name2') {
+		var numTest = /(\d+)\.\w+$/;
+		for (var i=0;i<files_.length;i++) {
+			files.push(files_[i]);
+		}
+		console.log(files);
+
+		files.sort(function(a, b) {
+			var nameA = a.name.match(numTest)[1],
+				nameB = b.name.match(numTest)[1];
+				if(sortType === 'name1')return(Number(nameA)-Number(nameB));
+				else return(Number(nameB)-Number(nameA));
+		});
+	}else{
+		files=files_;
+	}
+
+	function draw(num, img) {
+		if (direction === 'horizontal') {
+			canvas2d.drawImage(img, num * img.width, 0, img.width, img.height);
+		} else {
+			canvas2d.drawImage(img, 0, num * img.height, img.width, img.height);
+		}
+	}
+
+	for (var i = 0; i < files.length; i++) {
+		var img = new Image;
+		img.num = i;
+		img.name = files[i].name;
+		$(img).on('load', function() {
+			var img = this;
+			console.log(this.num);
+			console.log(this.name);
+			if (drawN === 0) {
+				if (direction === 'horizontal') {
+					canvas.width = files.length * img.width;
+					canvas.height = img.height;
+				} else {
+					canvas.width = img.width;
+					canvas.height = files.length * img.height;
+				}
+
+			}
+			draw(this.num, img);
+			drawN++;
+			if (drawN === files.length) {
+				var imgType = $('#imgType').val();
+				var data = canvas.toDataURL(IMGTYPE[imgType], $('#quality').val()/100);
+				var imgUrl = URL.createObjectURL(dataURLtoBlob(data));
+				var filename = $('#imgName').val() + '.' + imgType;
+				var down = document.createElement('a');
+				down.href = imgUrl;
+				down.download = filename;
+				var evt = document.createEvent("HTMLEvents");
+				evt.initEvent("click", false, false);
+				down.dispatchEvent(evt);
+				window.webkitURL.revokeObjectURL(imgUrl);
+
+				//alert('拼接完毕');
+			}
+		});
+		img.src = window.URL.createObjectURL(files[i]);
+
+	}
+
+}
